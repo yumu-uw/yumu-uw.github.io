fac0345d00aa1182b740f486e8a9392d73bcc2a4
yumuuw <yumuuw@gmail.com>
2023-11-18 22:37:11 +0900

    記事一覧にタグの表示を追加

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,11 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
+		tags: z.array(z.string()).min(1),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date(),
-		heroImage: z.string().optional(),
 	}),
 });
 
