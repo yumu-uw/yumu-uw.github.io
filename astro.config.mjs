bfd37b20bb56b216a12a86ba297f05efe325dfa8
yumuuw <yumuuw@gmail.com>
2024-09-15 20:32:37 +0900

    google analyticsを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import remarkBreaks from "remark-breaks";
 import remarkFlexibleCodeTitles from "remark-flexible-code-titles";
 
@@ -8,7 +9,15 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://blog.yumu-uw.com",
-	integrations: [mdx(), react()],
+	integrations: [
+		mdx(),
+		react(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 	markdown: {
 		remarkPlugins: [remarkBreaks, remarkFlexibleCodeTitles],
 	},
